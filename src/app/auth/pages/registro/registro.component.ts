5a05dc69945b709d4bb5c81a8c9617bbf6d04ab7
FDR <delrincon@gmail.com>
2022-08-06 19:21:30 -0300

    main routing added + fix bug css in all components

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
 
