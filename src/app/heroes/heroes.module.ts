d75e69a0fd511920b5d8840af7614bdb31428470
FDR <delrincon@gmail.com>
2022-08-11 12:01:59 -0300

    imagePipe from local OK

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -12,17 +12,22 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 
 
 @NgModule({
   declarations: [
+    // Components
     AgregarComponent,
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
     ListadoComponent,
-    HeroeTarjetaComponent
+    HeroeTarjetaComponent,
+
+    // Pipe
+    ImagenPipe
   ],
   imports: [
     CommonModule,
